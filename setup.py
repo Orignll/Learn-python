6c1d6bb98ae45b7883c34a9a056d43477152c4b5
Faril_Akbar <farilakbar385@gmail.com>
2023-12-10 19:51:31 +0700

    commit pertama

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='faril11',
+    version='0.1.0',
+    author='Faril Akbar',
+    author_email='farilakbar385@gmail.com',
+    description='jobsheet 13 farilakbar',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
